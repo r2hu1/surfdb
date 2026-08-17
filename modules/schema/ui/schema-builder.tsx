45074944df9297054b430fca5cb4d686a83740bb
r2hu1 <newasram86@gmail.com>
2026-08-18 00:15:06 +0530

    feat: update schema ui schema-builder.tsx

--- modules/schema/ui/schema-builder.tsx
+++ modules/schema/ui/schema-builder.tsx
@@ -2,6 +2,7 @@
 
 import { ReactFlowProvider } from "@xyflow/react";
 import { useKeyboard } from "../hooks/use-keyboard";
+import { useUIStore } from "../store/ui.store";
 import { SchemaCanvas } from "./canvas/schema-canvas";
 import { PropertiesPanel } from "./panels/properties-panel";
 import { CodePreview } from "./preview/code-preview";
@@ -10,19 +11,22 @@ import { SchemaToolbar } from "./toolbar/schema-toolbar";
 
 export function SchemaBuilder() {
   useKeyboard();
+  const sidebarOpen = useUIStore((s) => s.sidebarOpen);
+  const propertiesPanelOpen = useUIStore((s) => s.propertiesPanelOpen);
+  const codePreviewOpen = useUIStore((s) => s.codePreviewOpen);
 
   return (
     <ReactFlowProvider>
       <div className="flex h-full min-h-0 flex-col">
-        <SchemaToolbar />
+        {/*<SchemaToolbar />*/}
         <div className="flex min-h-0 flex-1">
-          <SchemaSidebar />
+          {sidebarOpen && <SchemaSidebar />}
           <main className="relative min-w-0 flex-1">
             <SchemaCanvas />
           </main>
-          <PropertiesPanel />
+          {propertiesPanelOpen && <PropertiesPanel />}
         </div>
-        <CodePreview />
+        {codePreviewOpen && <CodePreview />}
       </div>
     </ReactFlowProvider>
   );
